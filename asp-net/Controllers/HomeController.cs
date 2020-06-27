bbe5fb7ab42b09f17aa0082c562c370e82ff9a56
Henry <linhsonit@gmail.com>
2020-06-27 10:28:02 +0700

    add

---
+++ asp-net/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using asp_net.Models;
+using StackExchange.Redis;
+
+namespace asp_net.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IDatabase _database;
+
+        public HomeController(ILogger<HomeController> logger, IDatabase database)
+        {
+            _logger = logger;
+            _database = database;
+        }
+
+        public IActionResult Index()
+        {
+            string count = _database.StringGet("count");
+
+            _database.StringSet("count", count != null ? Convert.ToInt32(count) + 1 : 0);
+
+            return View("index", _database.StringGet("count"));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ asp-net/Database/HenryContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp_net.Database
+{
+    public class HenryContext : DbContext
+    {
+        public HenryContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+            => optionsBuilder.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Image { get; set; }
+
+
+        public List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+
+        public int BlogId { get; set; }
+
+        public Blog Blog { get; set; }
+    }
+}
